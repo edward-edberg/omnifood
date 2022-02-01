ccd41f8efb4afcaea3835b008a1476eceb8646e8
Edward Edberg <edward.edberg.halim@gmail.com>
2022-02-01 11:01:03 -0800

    add sticky navigation

--- js/script.js
+++ js/script.js
@@ -52,6 +52,27 @@ allLinks.forEach((link) => {
   });
 });
 
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
